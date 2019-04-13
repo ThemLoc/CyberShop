1858818d492282824ccd856f99e9fece5c401b30
LOCNT\Loc <ngthemloc@gmail.com>
2019-04-13 11:35:23 +0700

    add service package

--- CyberShop/src/main/java/com/cybershop/models/Admin.java
+++ CyberShop/src/main/java/com/cybershop/models/Admin.java
@@ -19,7 +19,6 @@ import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
-
 /**
  *
  * @author ngthe
