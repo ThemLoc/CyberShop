895b6a3a3b60101c022f6ffa5d3fcb45ae2d3947
kevinz2019 <kevinz1997st@gmail.com>
2019-04-30 16:44:27 +0700

    list brand on client

--- CyberShop/src/main/java/com/cybershop/controllers/WebsiteController.java
+++ CyberShop/src/main/java/com/cybershop/controllers/WebsiteController.java
@@ -1,14 +1,21 @@
 package com.cybershop.controllers;
 
+import com.cybershop.services.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class WebsiteController {
+    
+    @Autowired
+    private BrandService service;
 
     @RequestMapping(value = {"/website/home"}, method = RequestMethod.GET)
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("listBrand", service.getByAll());
         return "website/home";
     }
 
@@ -31,4 +38,5 @@ public class WebsiteController {
     public String check_out() {
         return "website/checkout";
     }
+    
 }
